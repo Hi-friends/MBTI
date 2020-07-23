8f9627a6d297187c5c7ca43eed72f28bed2c698a
Hi-friends <38758444+Hi-friends@users.noreply.github.com>
2020-07-23 22:43:12 +0900

    Update settings.py

--- mbti/settings.py
+++ mbti/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'h_pd8at#)rg29umn+81**(cd=&v=eizs)30y$8g$r=*7eerg6)'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,11 +77,11 @@ WSGI_APPLICATION = 'mbti.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.oracle',
-        'NAME': 'DBSERVER',
-        'USER': 'MBTI',
-        'PASSWORD': 'mbtidb',
-        'HOST': 'localhost',
-        'PORT': '1521',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
